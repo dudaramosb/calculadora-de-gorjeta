68b4a5d87ae104622bb296f2666ee2379cca63a2
maria eduarda <dudaramosbastos@gmail.com>
2024-04-01 10:04:15 -0300

    setimo vommit do html e primeiro do js

---
+++ script.js
@@ -0,0 +1,25 @@
+let conta = 0
+let pessoas = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
+
+function receberValorConta(evento){
+    conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+        const paragrafoErro = document.querySelector(".pessoas #erro")
+        const divErro = document.querySelector(".pessoas .input-box") 
+
+        if(evento.target.value === "0") {
+            paragrafoErro.computedStyleMap.display = "block"
+            divErro.setAttribute("id", "erro-di8v")     
+        } else {
+            paragrafoErro.computedStyleMap.display = "none"
+            divErro.setAttribute("id", "")
+        }
+}
